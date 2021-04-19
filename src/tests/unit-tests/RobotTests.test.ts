1ff07a471e15ce59b7d9b66525b3a028b434a839
pratikbhumkar <prateekbhumkar@gmail.com>
2021-04-19 21:44:52 +1000

    Missing getter and setter

--- src/tests/unit-tests/RobotTests.test.ts
+++ src/tests/unit-tests/RobotTests.test.ts
@@ -4,7 +4,7 @@ describe('Testing robot', () => {
     test('Testing if robot is initialized and has the size assigned.', () => {
         let robot = new Robot(5);
         expect(robot.getPlaced()).toEqual(false);
-        expect(robot.size).toEqual(5);
+        expect(robot.getSize()).toEqual(5);
         expect(robot.getDirection()).toEqual(undefined);
     });
 });
